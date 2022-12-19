d122512ab6865ea56206b54070d30dddb75e65a8
Aleksey <alexartsmm@gmail.com>
2022-12-19 17:44:56 +0300

    Add project files.

---
+++ FarpostCrowler/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace FarpostCrowler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxLimit = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timeFrom = new System.Windows.Forms.TextBox();
+            this.timeTo = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timeToSetFrom = new System.Windows.Forms.TextBox();
+            this.timeToSetTo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Контроль ставок";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(13, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Лимит";
+            // 
+            // boxLimit
+            // 
+            this.boxLimit.Location = new System.Drawing.Point(73, 46);
+            this.boxLimit.Name = "boxLimit";
+            this.boxLimit.PlaceholderText = "1000";
+            this.boxLimit.Size = new System.Drawing.Size(100, 29);
+            this.boxLimit.TabIndex = 5;
+            this.boxLimit.TextChanged += new System.EventHandler(this.boxLimit_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(15, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Время работы программы";
+            // 
+            // timeFrom
+            // 
+            this.timeFrom.Location = new System.Drawing.Point(15, 110);
+            this.timeFrom.Name = "timeFrom";
+            this.timeFrom.PlaceholderText = "11:05:00";
+            this.timeFrom.Size = new System.Drawing.Size(100, 29);
+            this.timeFrom.TabIndex = 7;
+            this.timeFrom.TextChanged += new System.EventHandler(this.timeFrom_TextChanged);
+            // 
+            // timeTo
+            // 
+            this.timeTo.Location = new System.Drawing.Point(120, 110);
+            this.timeTo.Name = "timeTo";
+            this.timeTo.PlaceholderText = "19:05:00";
+            this.timeTo.Size = new System.Drawing.Size(100, 29);
+            this.timeTo.TabIndex = 8;
+            this.timeTo.TextChanged += new System.EventHandler(this.timeTo_TextChanged);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(110, 302);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(89, 44);
+            this.startBtn.TabIndex = 9;
+            this.startBtn.Text = "Старт";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(205, 302);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(89, 44);
+            this.stopBtn.TabIndex = 10;
+            this.stopBtn.Text = "Стоп";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(15, 205);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Данные для входа";
+            // 
+            // loginBox
+            // 
+            this.loginBox.Location = new System.Drawing.Point(12, 232);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.PlaceholderText = "Login";
+            this.loginBox.Size = new System.Drawing.Size(208, 29);
+            this.loginBox.TabIndex = 12;
+            this.loginBox.TextChanged += new System.EventHandler(this.loginBox_TextChanged);
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(12, 267);
+            this.passBox.Name = "passBox";
+            this.passBox.PlaceholderText = "Password";
+            this.passBox.Size = new System.Drawing.Size(208, 29);
+            this.passBox.TabIndex = 13;
+            this.passBox.TextChanged += new System.EventHandler(this.passBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 146);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 21);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Интервал применения ставки";
+            // 
+            // timeToSetFrom
+            // 
+            this.timeToSetFrom.Location = new System.Drawing.Point(15, 170);
+            this.timeToSetFrom.Name = "timeToSetFrom";
+            this.timeToSetFrom.PlaceholderText = "От (в мсек)";
+            this.timeToSetFrom.Size = new System.Drawing.Size(100, 29);
+            this.timeToSetFrom.TabIndex = 15;
+            this.timeToSetFrom.TextChanged += new System.EventHandler(this.timeToSetFrom_TextChanged);
+            // 
+            // timeToSetTo
+            // 
+            this.timeToSetTo.Location = new System.Drawing.Point(120, 170);
+            this.timeToSetTo.Name = "timeToSetTo";
+            this.timeToSetTo.PlaceholderText = "До (в мсек)";
+            this.timeToSetTo.Size = new System.Drawing.Size(100, 29);
+            this.timeToSetTo.TabIndex = 16;
+            this.timeToSetTo.TextChanged += new System.EventHandler(this.timeToSetTo_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 357);
+            this.Controls.Add(this.timeToSetTo);
+            this.Controls.Add(this.timeToSetFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.loginBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.timeTo);
+            this.Controls.Add(this.timeFrom);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.boxLimit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Меню";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label3;
+        private TextBox boxLimit;
+        private Label label4;
+        private TextBox timeFrom;
+        private TextBox timeTo;
+        private Button startBtn;
+        private Button stopBtn;
+        private Label label5;
+        private TextBox loginBox;
+        private TextBox passBox;
+        private Label label2;
+        private TextBox timeToSetFrom;
+        private TextBox timeToSetTo;
+    }
+}
---
+++ FarpostCrowler/Form1.cs
@@ -0,0 +1,305 @@
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace FarpostCrowler
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver driver = new ChromeDriver();
+        Random rnd = new Random();
+
+        public static string loginField { get; set; }
+        public static string passField { get; set; }
+        public static double limitFrom { get; set; }
+        public static double limitTo { get; set; }
+        public static double fullLimit { get; set; }
+        public static string TimeStart { get; set; }
+        public static string TimeEnd { get; set; }
+        public static int TimeSetFrom { get; set; }
+        public static int TimeSetTo { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Task.Factory.StartNew(ConsoleOpen);
+        }
+        private void ConsoleOpen()
+        {
+            if (AllocConsole())
+            {
+                System.Console.WriteLine("For close, print - exit.");
+                while (true)
+                {
+                    string output = Console.ReadLine();
+                    if (output == "exit")
+                        break;
+                    Action action = () => passBox.Text += output + Environment.NewLine;
+                    if (InvokeRequired)
+                        Invoke(action);
+                    else
+                        action();
+                }
+                //FreeConsole();
+            }
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool FreeConsole();
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            TimeCheck.TimeOfTheJobCheck(TimeStart,TimeEnd);
+
+            // Запуск браузера
+            driver.Navigate().GoToUrl("https://www.farpost.ru/");
+            Thread.Sleep(rnd.Next(3500, 5500));
+
+            List<IWebElement> login = driver.FindElements(By.XPath("//a[contains(@class, 'login')]")).ToList();
+            if(login.Count > 0)
+            {
+                // Нажать на вход
+                IWebElement lonelogin = login.First();
+                lonelogin.Click();
+                Thread.Sleep(rnd.Next(7500, 14500));
+
+                // Логин 
+                List<IWebElement> loginField = driver.FindElements(By.XPath("//input[contains(@class, 'text')]")).ToList();
+                if(loginField.Count > 0)
+                {
+                    IWebElement lone = loginField.First();
+                    lone.Click();
+
+                    Thread.Sleep(rnd.Next(3500, 4500));
+
+                    ActionsOnWeb.TextEnter(lone, loginBox.Text.ToString(), 50, 250);
+                }
+
+                // Пароль
+                List<IWebElement> passField = driver.FindElements(By.XPath("//div[contains(@class, 'controlBody')]/input[contains(@name, 'password')]")).ToList();
+                if (passField.Count > 0)
+                {
+                    IWebElement lone = passField.First();
+                    lone.Click();
+
+                    Thread.Sleep(rnd.Next(3500, 4500));
+
+                    ActionsOnWeb.TextEnter(lone, passBox.Text.ToString(), 50, 250);
+                }
+
+                // Кнопка входа
+                List<IWebElement> enterBtn = driver.FindElements(By.XPath("//div[contains(@class, 'submitBlock')]/button[contains(@type, 'submit')]")).ToList();
+                if (enterBtn.Count > 0)
+                {
+                    IWebElement lone = enterBtn.First();
+                    lone.Click();
+
+                    Thread.Sleep(rnd.Next(3500, 4500));
+
+                    // Если появилось сообщение - "получить смс"
+                    List<IWebElement> btnLogin = driver.FindElements(By.XPath("//button[contains(@type, 'submit')]")).ToList();
+                    if(btnLogin.Count > 0)
+                    {
+                        IWebElement getSms = btnLogin.First();
+                        getSms.Click();
+
+                        Thread.Sleep(rnd.Next(3500, 4500));
+
+                        MessageBox.Show("Пауза для ввода смс");
+                    }
+                }
+
+                List<string> allAdvInCabLst = new List<string>();
+
+                // Составляем список всего что есть в личном кабинете
+                driver.Navigate().GoToUrl("https://www.farpost.ru/personal/actual/bulletins");
+                Thread.Sleep(rnd.Next(3500, 4500));
+
+                // Цикл проверки объявлений и изменения ставок
+                while(true)
+                {
+                    //div[contains(@class, 'bull-item-content__content-wrapper')]
+                    // Сначала кидаем в список все что есть на странице
+                    List<IWebElement> advs = driver.FindElements(By.XPath("//div[contains(@class, 'bull-item-content__subject-container')]/a")).ToList();
+                    foreach(var elm in advs)
+                    {
+                        allAdvInCabLst.Add(elm.Text);
+                    }
+
+                    // Переключаем страницу
+
+                    // Переходим на каждое объявление и меняем ставки, согласно настройкам в программе
+                    foreach(var elm in allAdvInCabLst)
+                    {
+                        List<IWebElement> loneElms = driver.FindElements(By.XPath(String.Format("//div[contains(@class, 'bull-item-content__subject-container')]/a[contains(text(), '{0}')]", elm))).ToList();
+                        if(loneElms.Count > 0)
+                        {
+                            IWebElement one = loneElms.First();
+                            one.Click();
+                        }
+
+                        // Проверяем наличие кнопки "приклеить" - если она есть, значит объявление активно
+                        List<IWebElement> stickBtn = driver.FindElements(By.XPath("//a[contains(@class, 'serviceStick')]")).ToList();
+                        if(stickBtn.Count > 0)
+                        {
+                            IWebElement lone = stickBtn.First();
+                            lone.Click();
+
+                            Thread.Sleep(rnd.Next(6500, 7500));
+
+                            // Здесь проверяем соответствие ставкам
+                            Console.WriteLine("Жмем - приклеить");
+
+                            // limitFrom - ставка от
+                            // limitTo - ставка до
+                            // fullLimit - верхняя граница
+
+                            // Читаем ставки на сайте
+                            List<IWebElement> setForFirstPlace = driver.FindElements(By.XPath("//div[contains(@class, 'stick-applier-steps__details')]/div/a/span[1]")).ToList();
+                            double setForFirstPlaceTxt = Convert.ToDouble(setForFirstPlace.First().Text);
+                            double minPriceSetTxt = Convert.ToDouble(setForFirstPlace.Last().Text);
+
+                            // Проверяем лимит
+                            if (setForFirstPlaceTxt <= fullLimit)
+                            {
+                                Console.WriteLine("Ставка за первое место в указанном диапазоне, ставим ставку за первое место + 2руб");
+                                IWebElement set = driver.FindElement(By.XPath("//input[contains(@name, 'stickPrice')]"));
+                                set.Click();
+
+                                Thread.Sleep(rnd.Next(1500, 2500));
+
+                                Clipboard.SetText(Convert.ToString(setForFirstPlaceTxt + 2));
+                                // Ставка
+                                IWebElement stickSet = driver.FindElement(By.XPath("//input[contains(@name, 'stickPrice')]"));
+                                stickSet.SendKeys(OpenQA.Selenium.Keys.Control + "a");
+                                stickSet.SendKeys(OpenQA.Selenium.Keys.Control + "delete");
+                                stickSet.SendKeys(OpenQA.Selenium.Keys.Control + "v");
+
+                                Thread.Sleep(rnd.Next(1500, 2500));
+
+                                // Жмем приклеить
+                                IWebElement stickBtnF = driver.FindElement(By.XPath("//button[contains(@type, 'submit')]"));
+                                stickBtnF.Click();
+                            }
+                            else
+                            {
+                                Console.WriteLine("По объявлению превышен лимит, ничего не делаем с ним");
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Объявление не активно, шаг назад");
+                            driver.Navigate().GoToUrl("https://www.farpost.ru/personal/all/bulletins");
+                            Thread.Sleep(rnd.Next(1500, 2500));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void boxLimit_TextChanged(object sender, EventArgs e)
+        {
+            fullLimit = Convert.ToDouble(boxLimit.Text);
+        }
+
+        private void timeFrom_TextChanged(object sender, EventArgs e)
+        {
+            TimeStart = timeFrom.Text;
+        }
+
+        private void timeTo_TextChanged(object sender, EventArgs e)
+        {
+            TimeEnd = timeTo.Text;
+        }
+
+        private void loginBox_TextChanged(object sender, EventArgs e)
+        {
+            loginField = loginBox.Text;
+        }
+
+        private void passBox_TextChanged(object sender, EventArgs e)
+        {
+            passField = passBox.Text;
+        }
+
+        private void timeToSetFrom_TextChanged(object sender, EventArgs e)
+        {
+            TimeSetFrom = Convert.ToInt32(timeToSetFrom.Text);
+        }
+
+        private void timeToSetTo_TextChanged(object sender, EventArgs e)
+        {
+            TimeSetTo = Convert.ToInt32(timeToSetTo.Text);
+        }
+    }
+
+    public class ActionsOnWeb
+    {
+        /// <summary>
+        /// Побуквенный ввод текста
+        /// </summary>
+        /// <param name="elm"> В какой элемент отправлять текст: поле для ввода, например</param>
+        /// <param name="Variable"> Текст для отправки</param>
+        /// <param name="speedFrom"> Скорость "от" в мсек</param>
+        /// <param name="speedTo"> Скорость "до" в мсек</param>
+        public static void TextEnter(IWebElement elm, string Variable, int speedFrom, int speedTo)
+        {
+            Random rnd = new Random();
+
+            for (int m = 0; m < Variable.Length; m++)
+            {
+                elm.SendKeys(Variable[m].ToString());
+                Thread.Sleep(rnd.Next(speedFrom, speedTo));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Проверка таблицы
+    /// </summary>
+    public class TimeCheck
+    {
+        public static DateTime nowTime { get; set; } = DateTime.Now;
+
+        public static void TimeOfTheJobCheck(string startJob, string endJob)
+        {
+            nowTime = DateTime.ParseExact(endJob, "HH:mm:ss", CultureInfo.GetCultureInfo("ru-RU"));
+            DateTime dto = DateTime.ParseExact(startJob, "HH:mm:ss", CultureInfo.GetCultureInfo("ru-RU"));
+
+            Console.WriteLine("Время старта работы: " + dto);
+
+            // Считаем разницу между текущим временем и установленным в таблице
+
+            double diff = nowTime.Subtract(dto).TotalMilliseconds;
+            Console.WriteLine("Разница между датами: " + diff);
+            Console.WriteLine("Ожидание начала задания: " + diff);
+
+            while (true)
+            {
+                if (diff < 0)
+                {
+                    Console.WriteLine("Ожидаем время начала задания");
+                    Thread.Sleep(10000);
+                }
+                else
+                {
+                    Console.WriteLine("Наступило время начала задания - запускаем");
+                    //Thread.Sleep(Convert.ToInt32(diff));
+                    break;
+                }
+            }
+        }        
+    }
+}
